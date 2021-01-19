d8f8fd98152bc7e3322df7c6e7f9a846567c6e0e
sonu ishaq <sonuishaq67@gmail.com>
2021-01-19 13:53:55 +0530

    testing auth

--- backend/db.py
+++ backend/db.py
@@ -7,7 +7,7 @@ from flask.cli import with_appcontext
 def get_db():
     if "db" not in g:
         g.db = sqlite3.connect(
-            current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
+            "sqlite_db", detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
 
@@ -21,13 +21,13 @@ def close_db(e=None):
 
 def init_db():
     db = get_db()
-    with current_app.open_resource("schema.sql") as f:
+
+    with current_app.open_resource("./backend/schema.sql") as f:
         db.executescript(f.read().decode("utf8"))
 
-@click.command("init-db")
+@click.command()
 @with_appcontext
 def init_db_command():
-    """Clear the existing data and create new tables."""
     init_db()
     click.echo("Initialized the database.")
 
--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,4 @@ from start import app
 import os
 
 if __name__ == "__main__":
-    os.system("npm i && npm run build")
-    os.system("rm -rfv node_modules")
     app.run()
